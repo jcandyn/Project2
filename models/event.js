c47cdb86affaad39d0b919db5dab6f1262ee69d4
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-09-18 16:31:43 -0400

    changed routes to show ejs pages and created the ejs page for showing events and creating one

--- models/event.js
+++ models/event.js
@@ -56,13 +56,13 @@ module.exports = function (sequelize, DataTypes) {
     },
   });
 
-  Event.associate = function (models) {
-    Event.belongsTo(models.User, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  };
+  // Event.associate = function (models) {
+  //   Event.belongsTo(models.User, {
+  //     foreignKey: {
+  //       allowNull: false
+  //     }
+  //   });
+  // };
 
   return Event;
 };
