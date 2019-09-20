0e9ea4db6ed146ef9c0dc06bea636b02263a101b
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-09-19 21:22:47 -0400

    added conditions for making log/sign up dissapear once user is logged in

--- public/js/all_events.js
+++ public/js/all_events.js
@@ -39,7 +39,7 @@ function noEvents(id){
     allEvents.empty();
     var messageH2 = $("<h2>");
     messageH2.css({ "text-align": "center", "margin-top": "50px" });
-    messageH2.html("No events yet" + partial + ", navigate <a href='/cms" + query +
+    messageH2.html("No events yet" + partial + ", navigate <a href='/create" + query +
     "'>here</a> in order to get started.");
     allEvents.append(messageH2);
 }
